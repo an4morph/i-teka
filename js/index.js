e1dee374ed3662a8fa97211c60cf71a66673ef18
an4morph <avgustin456@gmail.com>
2019-05-29 17:58:15 +0600

    Beautify js

--- js/index.js
+++ js/index.js
@@ -15,8 +15,12 @@ require('slick-carousel/slick/slick-theme.scss');
 require('jquery-ui-dist/jquery-ui');
 require('jquery-ui-dist/jquery-ui.css');
 
-const RevealGlobal = () => {
-    window.sr = ScrollReveal({ reset: true })
+
+// вся логика разделена постранично, чтобы на каждой 
+// странице генерились уникальные события
+
+$(document).ready(function () {
+    const searchSource = ['abc', 'aaaaa', 'aaaaa1', 'aaaaa2', 'aaaaa3', 'cde', 'efg', 'ghi', 'ikl']
     ScrollReveal().reveal('.reveal', {
         delay: 150,
         duration: 1000,
@@ -25,12 +29,6 @@ const RevealGlobal = () => {
         opacity: 0,
         reset: false
     })
-}
-
-
-$(document).ready(function () {
-    const searchSource = ['abc', 'aaaaa', 'aaaaa1', 'aaaaa2', 'aaaaa3', 'cde', 'efg', 'ghi', 'ikl']
-    RevealGlobal();
     $(".slider").slick({ dots: true });
     
     Header()
@@ -38,25 +36,35 @@ $(document).ready(function () {
     if ($('.faq-page').length) {
         createExpand('.faq-item', '.faq-item h2')
     }
+
+    //=========================================== DISEASES (Заболевания) ===========================================//
     if ($('.diseases-page').length) {
         searchAutocomplete('#diseases-page__search', searchSource)
         createExpand('.diseases-page .list .var', '.diseases-page .list .var h2')
     }
+
+
+    //=========================================== MED SERVICES (Процедуры) ===========================================//
     if ($('.med-services-page').length) {
         searchAutocomplete('#med-services-page__search', searchSource)
         createExpand('.med-services-page .list .var', '.med-services-page .list .var h2')
     }
+
+
+    //=========================================== DRUGS (Лекарства) ===========================================//
     if ($('.drugs-page').length) {
         searchAutocomplete('#drugs-page__search', searchSource)
         createExpand('.drugs-page .list .var', '.drugs-page .list .var h2')
     }
-    // if ($('.drugs-search-page').length) {
-    //     createSidebar('.drugs-search-page-body .sort-sidebar', '#drugs-search__sort-btn')
-    //     createSidebar('.drugs-search-page-body .filter-sidebar', '#drugs-search__filter-btn')
-    // }
+
+
+    //=========================================== DOCTORS (Врачи) ===========================================//
     if ($('.doctors-page').length) {
         searchAutocomplete('#doctors-page__search', searchSource)
         createExpand('.doctors-page .list .var', '.doctors-page .list .var h2')
+    }
+    if ($('.doctors-item-page').length) {
+        // no js
     }
     if ($('.doctors-search-page').length) {
         createSidebar('.doctors-search-page-body .sort-sidebar', '#doctors-search__sort-btn')
@@ -64,18 +72,30 @@ $(document).ready(function () {
         modal('.doctors-search-page-body', '.report-modal', '.report-btn')
         modal('.doctors-search-page-body', '.booking-modal', '.booking-btn')
     }
+    
+
+    //=========================================== PHARMACY (Аптеки) ===========================================//
     if ($('.pharmacy-page').length) {
         searchAutocomplete('#pharmacy-page__search', searchSource)
         createExpand('.pharmacy-page .list .var', '.pharmacy-page .list .var h2')
+    }
+    if ($('.pharmacy-item-page').length) {
+        // no js
     }
     if ($('.pharmacy-search-page').length) {
         createSidebar('.pharmacy-search-page-body .sort-sidebar', '#pharmacy-search__sort-btn')
         createSidebar('.pharmacy-search-page-body .filter-sidebar', '#pharmacy-search__filter-btn')
         modal('.pharmacy-search-page-body', '.report-modal', '.report-btn')
     }
+
+
+    //=========================================== MED CENTERS (Клиники) ===========================================//
     if ($('.med-centers-page').length) {
         searchAutocomplete('#med-center-page__search', searchSource)
         createExpand('.med-centers-page .list .var', '.med-centers-page .list .var h2')
+    }
+    if ($('.med-centers-item-page').length) {
+        // no js
     }
     if ($('.med-centers-search-page').length) {
         createSidebar('.med-centers-search-page-body .sort-sidebar', '#med-centers-search__sort-btn')
