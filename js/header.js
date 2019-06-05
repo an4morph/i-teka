13046514446bed123558395685043167d6543cfc
an4morph <avgustin456@gmail.com>
2019-06-05 15:54:32 +0600

    desktop hover

--- js/header.js
+++ js/header.js
@@ -4,7 +4,13 @@ const createDropdown = require('./utils/dropdown');
 
 module.exports = () => {
   $("header").sticky({ topSpacing: 0 });
-  createDropdown('#burger1-icon', '#burger1-menu')
-  createDropdown('#burger2-icon', '#burger2-menu')
-  createDropdown('#logout-icon', '#logout-menu')
+  
+  $("header menu li.menu-item").each((index, item) => {
+    $(item).hover(() => {
+      $(item).find('.onhover').css('display', 'flex')
+    }, () => {
+      //console.log($(item).find('.onhover')[0])
+    })
+
+  })
 }
